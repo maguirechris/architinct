a4223afc8f77bdf0ecdacbbf728179ff3664ff41
Jarosik-Grace <gracejarosik05@gmail.com>
2025-06-03 13:58:22 -0400

    Audio/SFX implementations

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -6,37 +6,58 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour, IMenu
 {
     public GameObject pauseMenu;
+    public AudioSource audioSource;
+    public AudioClip menuToggle;
+    public AudioClip buttonClick;
 
-    void Update() {
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Escape) && !PlaceBuilding.Instance.isPlacing)
         {
             if (!MenuManager.IsAnyMenuOpen())
             {
-                MenuManager.OpenOverlayMenu(this); ;
+                MenuManager.OpenOverlayMenu(this);
+                PlaySound(menuToggle);
             }
             else
             {
                 MenuManager.CloseOverlayMenu();
+                PlaySound(menuToggle);
             }
         }
     }
 
-    public void Open() {
+    public void Open()
+    {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        PlaySound(menuToggle);
     }
 
-    public void Close() {
+    public void Close()
+    {
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        PlaySound(menuToggle);
     }
 
-    public void ClickAndLoadMainMenu() {
+    public void ClickAndLoadMainMenu()
+    {
         Time.timeScale = 1;
+        PlaySound(buttonClick);
         Invoke("LoadMainMenu", 0.3f);
     }
 
-    public void LoadMainMenu() {
+    public void LoadMainMenu()
+    {
         SceneManager.LoadScene("Main Menu");
+    }
+    
+    private void PlaySound(AudioClip clip) {
+        if (audioSource != null && clip != null)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
     }
 }
